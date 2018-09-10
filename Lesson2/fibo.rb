1455e71d94572bb385d5f50d10ea1a88d51c8d27
Roman Ustiantcev <liberrom.hse@gmail.com>
2018-09-10 23:40:11 +0200

    lesson 2, task 3 - added

---
+++ Lesson2/fibo.rb
@@ -0,0 +1,14 @@
+def get_fibo
+  array = []
+  for i in 0..99
+    array[i] = i
+  end
+  array[0] = 0
+  array[1] = 1
+  for i in 2..99
+    array[i] = array[i - 1] + array[i - 2]
+  end
+  array
+end
+
+puts get_fibo
