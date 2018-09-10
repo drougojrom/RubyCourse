d4925c3ff7bc371b21ca2146987b908e9fc763f9
Roman Ustiantcev <roman.ustuantsev@recordunion.com>
2018-09-10 15:59:39 +0200

    task 2 - added

---
+++ Lesson1/task2.rb
@@ -0,0 +1,8 @@
+puts "Let's calculate the area of a triangle. Please, enter it's height"
+height = gets.to_i
+puts "Now enter the base value"
+base = gets.to_i
+
+result = (base * height) / 2
+
+puts "The result is: #{result}"
