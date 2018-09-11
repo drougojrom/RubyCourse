184b21981af88c4b7620d443fda2a4210063323a
Roman Ustiantcev <liberrom.hse@gmail.com>
2018-09-11 21:41:32 +0200

    days count - update, fibo - refactored

--- Lesson2/days_count.rb
+++ Lesson2/days_count.rb
@@ -1,51 +1,22 @@
-class DateCounter
-
-  def initialize(day, month, year)
-    @day = day
-    @month = month
-    @year = year
-  end
-
-  def get_date_counter
-    month_range = (1..@month)
-    hash = fill_hash(@year, month_range)
-    previous_days_count = 0
-    hash.each do |k, v|
-      if k < @month
-        previous_days_count += v
-      end
-    end
-    previous_days_count += @day
-    previous_days_count
-  end
-
-  private
-  
-  def fill_hash(year, month_range)
-    hash = {}
-    puts month_range
-    1.upto(month_range.last) do |month|
-      hash[month] = days_in_month(year, month)
-    end
-    hash
-  end
-
-  def days_in_month(year, month)
-    if is_leap?(year) && month == 2
-      28
-    elsif month % 2 == 0
-      30
-    else
-      31
+def get_date_counter(day, month, year)
+  previous_days_count = 0
+  @months[1] += 1 if is_leap?(year)
+  @months.each_with_index do |value, index|
+    if index < month
+      previous_days_count += value
     end
   end
-
-  def is_leap?(year)
-    return false unless year % 4 == 0
-    return true unless year % 100 == 0
-    year % 400 == 0
-  end
+  previous_days_count += day
+  previous_days_count
 end
+
+def is_leap?(year)
+  return false unless year % 4 == 0
+  return true unless year % 100 == 0
+  year % 400 == 0
+end
+
+@months = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 
 puts "Enter the days value"
 day = gets.to_i
@@ -54,5 +25,4 @@ month = gets.to_i
 puts "Enter the years value"
 year = gets.to_i
 
-d = DateCounter.new(day, month, year)
-puts "The selected day number is #{d.get_date_counter}"
+puts "The selected day number is #{get_date_counter(day, month-1, year)}"
--- Lesson2/fibo.rb
+++ Lesson2/fibo.rb
@@ -1,12 +1,8 @@
-# funny task, took me some time to figure it out :)
-
 def get_fibo
   array = [0, 1]
-  current_last = array.last
-  while current_last <= 100
-    last_numbers = array.last(2)
-    current_last = last_numbers[1] + last_numbers[0]
-    array << current_last unless current_last >= 100
+  for i in 2..20
+    current_last = array[i - 2] + array[i - 1]
+    current_last <= 100 ? array << current_last : break
   end
   array
 end
