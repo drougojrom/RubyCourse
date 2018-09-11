57cda643a03ee03bc342c3e7b273fc0c3ea940e0
Roman Ustiantcev <roman.ustuantsev@recordunion.com>
2018-09-11 16:36:33 +0200

    task 5 - fixed

--- Lesson2/days_count.rb
+++ Lesson2/days_count.rb
@@ -1,5 +1,3 @@
-require_relative 'fill_hash_helper'
-
 class DateCounter
 
   def initialize(day, month, year)
@@ -9,7 +7,7 @@ class DateCounter
   end
 
   def get_date_counter
-    month_range = 1..@month
+    month_range = (1..@month)
     hash = fill_hash(@year, month_range)
     previous_days_count = 0
     hash.each do |k, v|
@@ -17,9 +15,35 @@ class DateCounter
         previous_days_count += v
       end
     end
-
     previous_days_count += @day
     previous_days_count
+  end
+
+  private
+  
+  def fill_hash(year, month_range)
+    hash = {}
+    puts month_range
+    1.upto(month_range.last) do |month|
+      hash[month] = days_in_month(year, month)
+    end
+    hash
+  end
+
+  def days_in_month(year, month)
+    if is_leap?(year) && month == 2
+      28
+    elsif month % 2 == 0
+      30
+    else
+      31
+    end
+  end
+
+  def is_leap?(year)
+    return false unless year % 4 == 0
+    return true unless year % 100 == 0
+    year % 400 == 0
   end
 end
 
