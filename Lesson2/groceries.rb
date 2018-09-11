749a1b7ee12165689b625fcf742f93721596ed56
Roman Ustiantcev <roman.ustuantsev@recordunion.com>
2018-09-11 16:55:58 +0200

    task 6 - updated

--- Lesson2/groceries.rb
+++ Lesson2/groceries.rb
@@ -6,18 +6,22 @@ def fill_the_item(name)
   price = gets.to_f
   puts "How many item did you select?"
   numer_of_items = gets.to_i
-  @groceries[name] = {price => numer_of_items}
+  @groceries[name] = {price: price, number_of_items: numer_of_items}
 end
 
 def get_all_your_cart_items
-  puts "You cart is: #{@groceries}"
+  puts "You cart has: "
+  @groceries.each do |k, v|
+    puts k
+    puts "You've taken #{v[:number_of_items]} of them and each one costs #{v[:price]}"
+  end
 end
 
 def get_every_item_total_price
   total_value = 0
   @groceries.each do |k, v|
     puts "Your item name is #{k}"
-    item_value = v.keys.first * v.values.first
+    item_value = v[:price] * v[:number_of_items]
     total_value += item_value
     puts "The value of selected item is #{item_value}"
   end
