c70c55db1709e444085e4cf03c247223e0d858d9
Roman Ustiantcev <roman.ustuantsev@recordunion.com>
2018-09-12 10:20:45 +0200

    fibo - updated

--- Lesson2/fibo.rb
+++ Lesson2/fibo.rb
@@ -1,8 +1,10 @@
 def get_fibo
   array = [0, 1]
-  for i in 2..20
+  i = 2
+  loop do
     current_last = array[i - 2] + array[i - 1]
     current_last <= 100 ? array << current_last : break
+    i+= 1
   end
   array
 end
