643f994a01f17f38267130a68de6e759b741e380
Roman Ustiantcev <liberrom.hse@gmail.com>
2018-09-14 22:24:36 +0200

    create - added

--- Lesson4/main.rb
+++ Lesson4/main.rb
@@ -3,14 +3,148 @@ require './cargo_train.rb'
 require './passenger_wagon.rb'
 require './passenger_train.rb'
 
-while true
-  puts "What do you want to add?"
-  input = gets.chomp.downcase
+@created_trains = Array.new
+@created_stations = Array.new
+@created_wagons = Array.new
+@created_routes = Array.new
+
+def create
+  puts "What do you want to create?"
+  puts "1 - Create a Train"
+  puts "2 - Create a Station"
+  puts "3 - Create a Wagon"
+  puts "4 - Create a Route"
+
+  input = gets.to_i
 
   case input
-  when "train"
-    puts "train"
+  when 1..4
+    create_for(input)
   else
-    puts "Wow"
+    puts "Invalid input"
+  end
+end
+
+def show_stations
+  index = 1
+  @created_stations.each do |station|
+    puts "#{index} - #{station.name}"
+    index += 1
+  end
+end
+
+def show_trains
+  index = 1
+  @created_trains.each do |train|
+    puts "#{index} - #{train.number}"
+    index += 1
+  end
+end
+
+def show_routes
+  index = 1
+  @created_routes.each do |route|
+    puts "#{index} - #{route.get_stations_list}"
+  end
+end
+
+def show_wagons
+  index = 1
+  @created_wagons.each do |wagon|
+    puts "#{index} - #{wagon}"
+  end
+end
+
+def create_for(input)
+  case input
+  when 1
+    puts "What's your train number?"
+    number = gets.to_i
+    puts "What's your train type? Select 1 for pass and 2 for cargo"
+    type = gets.to_i
+    @train = type == 1 ? PassengerTrain.new(number) : CargoTrain.new(number)
+    @created_trains << @train
+    puts "You've created a new train with id: #{@train.object_id}"
+  when 2
+    puts "What's the name of your station?"
+    name = gets.chomp
+    @station = Station.new name
+    @created_stations << @station
+    puts "You've created a new station with id: #{@station.object_id}"
+  when 3
+    puts "Which type of wagon do you want to create? Select 1 for pass and 2 for cargo"
+    type = gets.to_i
+    @wagon = type == 1 ? PassengerWagon.new : CargoWagon.new
+    @created_wagons << @wagon
+    puts "You've created a new wagon with id: #{@wagon.object_id}"
+  when 4
+    puts "In order to create a route, please select its start"
+    show_stations
+    start_index = gets.to_i
+    start_station = @created_stations[start_index - 1]
+    puts "You also need to choose its end station"
+    show_stations
+    end_index = gets.to_i
+    end_station = @created_stations[end_index - 1]
+    if start_station == end_station
+      puts "You've selected the same station twice"
+      return
+    else
+      @route = Route.new start_station, end_station
+      @created_routes << @route
+      puts "You've created a new route with id: #{@route.object_id}"
+    end
+  else
+    puts "wow"
+  end
+end
+
+
+def add
+end
+
+def move
+end
+
+def show
+  puts '1 - Stations, 2 - Trains, 3 - Wagons, 4 - Routes'
+  input = gets.to_i
+  case input
+  when 1
+    show_stations
+  when 2
+    show_trains
+  when 3
+    show_wagons
+  when 4
+    show_routes
+  else
+    puts "Invalid input"
+  end
+end
+
+while true
+  puts "What do you want to do?"
+  puts "1 - Create Train/Station/Route/Wagon"
+  puts "2 - Add Stations to Routes, wagons to trains, Routes to trains"
+  puts "3 - Move train"
+  puts "4 - Show list of stations or trains for routes"
+  puts "5 - quit"
+
+  input = gets.to_i
+
+  case input
+  when 1
+    create
+  when 2
+    add
+  when 3
+    move
+  when 4
+    show
+  when 5
+    break
+  else
+    puts "Invalid input"
   end
 end
