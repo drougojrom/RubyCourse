f3b45bf95bf53d64bc84e34ec3be0423136d83b6
Roman Ustiantcev <roman.ustuantsev@recordunion.com>
2018-09-17 16:31:10 +0200

    all stations - added

--- Lesson5/station.rb
+++ Lesson5/station.rb
@@ -2,9 +2,16 @@ class Station
 
   attr_reader :name
 
+  @@stations = []
+
+  def self.all
+    @@stations
+  end
+
   def initialize(name)
     @name = name
     @trains = []
+    @@stations << self
   end
 
   def name
