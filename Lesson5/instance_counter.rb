80671d31a069f48833b2e957b414972cfc44fb99
Roman Ustiantcev <liberrom.hse@gmail.com>
2018-09-18 20:12:59 +0200

    found a bug

--- Lesson5/instance_counter.rb
+++ Lesson5/instance_counter.rb
@@ -1,20 +1,30 @@
 module InstanceCounter
+
+  @@instances = 0
+
+  def self.instances
+    @@instances
+  end
+
+  def self.register_instance
+    @@instances += 1
+  end
+
   def self.included(base)
     base.extend ClassMethods
     base.send :include, InstanceMethods
   end
 
   module ClassMethods
-    def self.instances
-      @@instances
+    def instances
+      InstanceCounter.instances
     end
   end
 
   module InstanceMethods
-    @@instances = 0
     protected
     def register_instance
-      @@instances += 1
+      InstanceCounter.register_instance
     end
   end
 end
