abcacf8ef80dd59d1a2c995d046b5aa8165154ab
Roman Ustiantcev <liberrom.hse@gmail.com>
2018-09-24 20:37:20 +0200

    passenger wagon - updated

--- Lesson7/passenger_wagon.rb
+++ Lesson7/passenger_wagon.rb
@@ -1,7 +1,24 @@
 require './wagon.rb'
 
 class PassengerWagon < Wagon
-  def initialize
+  attr_accessor :taken_seats
+
+  def initialize(seats)
     @type = :pass
+    @free_seats = seats
+    @taken_seats = 0
   end
+
+  def take_seat
+    @taken_seats += 1
+    @taken_seats = @free_seats if @taken_seats >= @free_seats
+  end
+
+  def seats_left
+    @free_seats - @taken_seats
+  end
+
+  private
+
+  attr_writer :free_seats
 end
